0a9bcdc7f902fb028e1ccf771f5e4bf49e26b838
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2022-07-08 05:17:55 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 
 using namespace std;
-bool power_of_2(int n){
-    return (n && !(n & (n-1)));
-}
-int main()
+bool getBit(int n , int pos )
 {
-   cout << power_of_2(1) << endl;
+   return ((n & ( 1 << pos ))!=0);
+}
+int setBit(int n , int pos ){
+return (n | (1<<pos ));
+}
+int unique_no(int arr[], int n ){
+int result =0;
+for(int i =0 ; i< 64 ; i++ ){
+    int sum = 0 ;
+    for(int j =0 ; j< n ; j++){
+        if(getBit(arr[j], i)){
+            sum++;
+        }
+    }
+    if(sum % 3 != 0){
+        result = setBit(result, i);
+    }
+}
+return result;
+}
+    int main()
+{
+   int arr[] = {1 ,1,2,4,4,3,4,1,2,3,2,3,9};
+   cout<<unique_no( arr , 13);
     return 0;
 }
